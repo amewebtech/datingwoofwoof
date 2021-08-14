787aa1a7062a05e5c67e22b90ac3ddd1c7f02b56
amewebtech <andy@amewebtech.co.uk>
2021-08-14 09:11:29 +0100

    end of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,18 @@
+using System.Data.Common;
+using System.Text;
+using System;
+using Microsoft.EntityFrameworkCore;
+using API.Entities;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
